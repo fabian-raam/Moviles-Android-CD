a653f3ec4b4faa2765abcb14f66a75de707e3388
Fabian Ramirez <fabianrq1@gmail.com>
2026-08-27 01:25:11 -0500

    Commit 1: Se creo file Carrito

---
+++ Semana02/app/src/main/java/com/ramirez/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.ramirez.lab02carritokotlin
+
+fun main(){
+    println("======================")
+    println("  CARRITO DE COMPRAS-TIENDA TECSUP  ")
+    println("======================")
+}
