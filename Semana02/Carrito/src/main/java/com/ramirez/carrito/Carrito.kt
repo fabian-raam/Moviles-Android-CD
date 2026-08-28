5959108f10ab2498a0ced016c9df00c6218f192a
Fabian Ramirez <fabianrq1@gmail.com>
2026-08-27 21:09:00 -0500

    COMMIT 3: Agregamos encapsulamiento en polimorfismo al carrito

--- Semana02/Carrito/src/main/java/com/ramirez/carrito/Carrito.kt
+++ Semana02/Carrito/src/main/java/com/ramirez/carrito/Carrito.kt
@@ -1,23 +1,35 @@
 package com.ramirez.carrito
 
 abstract class ItemCarrito(
-    val nombre: String,
-    val precio: Double
+    protected val nombre: String,
+    protected val precio: Double
 ) {
+
     abstract fun calcularImporte(): Double
+
+    fun obtenerNombre(): String {
+        return nombre
+    }
+
+    fun obtenerPrecio(): Double {
+        return precio
+    }
 }
 
 class Producto(
     nombre: String,
     precio: Double,
-    val cantidad: Int
+    private var cantidad: Int
 ) : ItemCarrito(nombre, precio) {
 
     override fun calcularImporte(): Double {
         return precio * cantidad
     }
-}
 
+    fun obtenerCantidad(): Int {
+        return cantidad
+    }
+}
 fun main() {
     println("=========================================")
     println(" CARRITO DE COMPRAS POO - TIENDA TECSUP ")
