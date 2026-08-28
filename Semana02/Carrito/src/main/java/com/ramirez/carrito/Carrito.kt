460ec59fa75f366994ed7140ea2eb40e29134336
Fabian Ramirez <fabianrq1@gmail.com>
2026-08-27 20:16:46 -0500

    Commit 1: Creacion de archivo y texto

---
+++ Semana02/Carrito/src/main/java/com/ramirez/carrito/Carrito.kt
@@ -0,0 +1,7 @@
+package com.ramirez.carrito
+
+fun main(){
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
