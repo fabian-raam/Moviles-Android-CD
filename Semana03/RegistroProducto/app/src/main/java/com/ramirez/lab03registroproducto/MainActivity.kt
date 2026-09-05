0d6c72523d4e6296a0f32f86de8c5d41284350db
Fabian Ramirez <fabianrq1@gmail.com>
2026-09-05 18:52:47 -0500

    Commit 1: Agrega encabezado con jerarquia tipografica”

---
+++ Semana03/RegistroProducto/app/src/main/java/com/ramirez/lab03registroproducto/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.ramirez.lab03registroproducto
+
+import android.R
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ramirez.lab03registroproducto.ui.theme.Lab03RegistroProductoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Lab03RegistroProductoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PantallaRegisrtro(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PantallaRegisrtro(modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "Nuevo producto",
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Text(
+            text = "Completa los datos y presiona Agregar",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.outline
+        )
+    }
+}
