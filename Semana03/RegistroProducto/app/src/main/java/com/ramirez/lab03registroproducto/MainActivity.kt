04218ae10fa0bc473a1bb56b26f02b6d177b1078
Fabian Ramirez <fabianrq1@gmail.com>
2026-09-05 19:19:47 -0500

    COMMIT 4: “Aplica reglas de diseno y mensaje de confirmacion”.

--- Semana03/RegistroProducto/app/src/main/java/com/ramirez/lab03registroproducto/MainActivity.kt
+++ Semana03/RegistroProducto/app/src/main/java/com/ramirez/lab03registroproducto/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -128,6 +129,10 @@ fun PantallaRegisrtro(modifier: Modifier = Modifier){
                         color = MaterialTheme.colorScheme.primary
                     )
                 }
+                Text(
+                    text = "✓ Producto registrado correctamente",
+                    color = Color(0xFF2E7D32)
+                )
             }
         }
     }
