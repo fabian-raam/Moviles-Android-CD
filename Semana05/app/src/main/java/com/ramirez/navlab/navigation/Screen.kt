6122889024892d725de6ad80b1f63e2dd4102007
Fabian Ramirez <fabianrq1@gmail.com>
2026-09-22 15:32:15 -0500

    Completa NavLab Semana05

---
+++ Semana05/app/src/main/java/com/ramirez/navlab/navigation/Screen.kt
@@ -0,0 +1,14 @@
+package com.ramirez.navlab.navigation
+
+sealed class Screen(val route: String) {
+
+    object Home : Screen("home")
+
+    object List : Screen("list")
+
+    object Profile : Screen("profile")
+
+    object Detail : Screen("detail/{itemId}") {
+        fun createRoute(itemId: Int) = "detail/$itemId"
+    }
+}
