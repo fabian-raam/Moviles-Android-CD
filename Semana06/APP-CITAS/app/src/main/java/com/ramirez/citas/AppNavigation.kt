14606d09481dbf26a5cffe596573c4546e77b008
Fabian Ramirez <fabianrq1@gmail.com>
2026-09-22 20:19:01 -0500

    Se agrego dependencia, se creo archivo AppNavigation y llamamos desde Main

---
+++ Semana06/APP-CITAS/app/src/main/java/com/ramirez/citas/AppNavigation.kt
@@ -0,0 +1,39 @@
+package com.ramirez.citas
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            Text(text = "Home Screen")
+        }
+        composable(Screen.DoctorDetail.route) {
+            Text(text = "Doctor Detail Screen")
+        }
+        composable(Screen.Appointment.route) {
+            Text(text = "Appointment Screen")
+        }
+        composable(Screen.Confirmation.route) {
+            Text(text = "Confirmation Screen")
+        }
+        composable(Screen.Appointments.route) {
+            Text(text = "Appointments Screen")
+        }
+        composable(Screen.MedicalHistory.route) {
+            Text(text = "Medical History Screen")
+        }
+        composable(Screen.Profile.route) {
+            Text(text = "Profile Screen")
+        }
+    }
+}
