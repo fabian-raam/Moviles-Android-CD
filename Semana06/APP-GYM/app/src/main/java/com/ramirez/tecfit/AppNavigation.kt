fb4412b4252524c5f020c500224de5a0c1f17486
Fabian Ramirez <fabianrq1@gmail.com>
2026-09-23 08:16:36 -0500

    Integra reservas con bottomBar

--- Semana06/APP-GYM/app/src/main/java/com/ramirez/tecfit/AppNavigation.kt
+++ Semana06/APP-GYM/app/src/main/java/com/ramirez/tecfit/AppNavigation.kt
@@ -2,7 +2,6 @@ package com.ramirez.tecfit
 
 import android.net.Uri
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
